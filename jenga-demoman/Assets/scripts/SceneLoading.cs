35a69b6dd0f75bbd2ebbcb95519014906ad61d0d
BossOfTheSauce1 <4028548@stu.highlineschools.org>
2023-06-02 08:53:40 -0700

    menu stuff

--- jenga-demoman/Assets/scripts/SceneLoading.cs
+++ jenga-demoman/Assets/scripts/SceneLoading.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoading : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
     }
 }
